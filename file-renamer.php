4eaa0d97f9bd56c07f1746af34746fc182cbb03d
Brandon Plentl <bplentl@gmail.com>
2017-04-14 09:37:43 -0500

    In beginning stages of logic for renaming files
    I am working on a script to rename files based on patterns

---
+++ file-renamer.php
@@ -0,0 +1,31 @@
+<?php
+
+$array_ignored_files = array(
+    ".",
+    "..",
+    basename(__FILE__)
+);
+
+$DocDirectory = "./.";   //Directory to be scanned
+$arrDocs = array_diff(scandir($DocDirectory), array('..', '.'));  //Scan the $DocDirectory and create an array list of all of the files and directories
+natcasesort($arrDocs);
+if( isset($arrDocs) && is_array($arrDocs) )
+{
+    foreach( $arrDocs as $a )   //For each document in the current document array
+    {
+        $fileName = pathinfo( $a, PATHINFO_FILENAME );
+        $fileExt = pathinfo($a, PATHINFO_EXTENSION);
+        $newFileExt = "php";
+
+        // Directory search and count
+        if( is_file($DocDirectory . "/" . $a) && !in_array($a,$array_ignored_files) && substr($a,strlen($a)-3,3) != ".db" && pathinfo($a, PATHINFO_EXTENSION) == "html")      //The "." and ".." are directories.  "." is the current and ".." is the parent
+        {
+            $oldname = $fileName . '.' . $fileExt;
+            $newname = $fileName . '.blade.' . $newFileExt;
+            echo "Renaming " . $oldname . " to " . $newname . "\n";
+            rename($oldname, $newname);
+        }
+    }
+}
+
+?>
